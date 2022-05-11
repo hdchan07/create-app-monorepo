4e983e26c5edd07fa5454a0bb61d9e49cfed91e6
陈华狄 <861883474@qq.com>
2022-05-11 14:24:17 +0800

    feat: @typescript-eslint/consistent-type-imports

--- packages/eslint-typescript/index.js
+++ packages/eslint-typescript/index.js
@@ -32,7 +32,7 @@ module.exports = {
 			},
 		],
 		"@typescript-eslint/type-annotation-spacing": ["error", {}],
-		"@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports", disallowTypeAnnotations: false }],
+		"@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports", disallowTypeAnnotations: false }],
 		"@typescript-eslint/consistent-type-definitions": ["error", "interface"],
 		"@typescript-eslint/consistent-indexed-object-style": ["error", "record"],
 		"@typescript-eslint/prefer-ts-expect-error": "error",
